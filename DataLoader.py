3a5faca16cdcc79531e7394593bb17e6bcb006dd
danulg <danulg@gmail.com>
2020-07-04 14:10:02 -0400

    Starting point for data Loader and augmentation code

--- DataLoader.py
+++ DataLoader.py
@@ -1,2 +1,10 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
+import re
+import os, shutil
+
+
+class IMDBDataSet():
+    
+if __name__ == "__main__":
+    data = IMDBDataSet()
+    train, test = data.load_normalized_data()
+    print(train)
