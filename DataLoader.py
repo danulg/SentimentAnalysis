a091eba7f59672a5ed620832c9e86ecf789b335b
danulg <danulg@gmail.com>
2020-07-05 09:32:53 -0400

    Load method for unspervised data constructed

--- DataLoader.py
+++ DataLoader.py
@@ -19,14 +19,15 @@ class IMDBDataSet():
                              'an', 'at', 'who', 'from', 'all', 'had', 'up', 'story', 'will', 'would', 'my', 'if',\
                              'only', 'see', 'can', 'it\'s', 'he', 'she', 'which', 'their', 'when', 'so', 'or', 'out'\
                              'some', 'just', 'this'}
-        self.imdb_dir = '/home/danul-g/IMDB'
-        self.unlabled_dir = os.path.join(self.imdb_dir, 'unsup')
+        self.imdb_dir = '/home/danulg/IMDB'
 
+    #Data augmentation methods
     def data_augmentation(self):
         pass
 
+    #Methods for looking at random data
     def random_original_review(self):
-        text, labels = self.load_original_test()
+        text, labels = self.load_original_train()
         print(len(text), len(labels))
         i = 0
         while(i<10):
@@ -34,11 +35,20 @@ class IMDBDataSet():
             print(text[j], labels[j])
             i+=1
 
+    def random_unsup_review(self):
+        text = self.load_unlabled()
+        print(len(text))
+        i = 0
+        while (i < 10):
+            j = random.randint(0, len(text) - 1)
+            print(text[j])
+            i += 1
+
     def random_augmented_review(self):
         pass
-
+    #Rough usage statistics: used to create lexicon for data augmentation
     def word_usage_analysis(self, label=1, num='35'):
-        text, labels = self.load_original_test()
+        text, labels = self.load_original_train()
         word_counter = Counter()
         for z in zip(text, labels):
             if z[1] == label or z[1]==2:
@@ -47,9 +57,10 @@ class IMDBDataSet():
                         word_counter[_]+=1
 
         print(word_counter.most_common(num))
-        #Add data for tokenizers? Can we use bokeh and scipy to get a nicer look at the data?
+        #Add data for tokenizers?
+        #Can we use bokeh and scipy to get a "nicer" look at the data?
 
-
+    #Data to more convinient form
     def labled_to_pickle(self, val='train'):
         labels = []
         text = []
@@ -69,28 +80,49 @@ class IMDBDataSet():
         dill.dump(text, open(val+'_text.pkd', 'wb'))
         dill.dump(labels, open(val+'_label.pkd', 'wb'))
 
+    def unlabled_to_pickle(self):
+        text = []
+        dir = os.path.join(self.imdb_dir, 'unsup')
+        for fname in os.listdir(dir):
+            with open(os.path.join(dir, fname)) as f:
+                text.append(f.read())
+        print(type(text), len(text))
+        dill.dump(text, open('unsup_text.pkd', 'wb'))
 
-    def load_augmented(self):
+    #Methods for loading data
+    def load_test(self):
+        with open('test_text.pkd', 'rb') as f:
+            text = dill.load(f)
+        with open('test_label.pkd', 'rb') as f:
+            labels = dill.load(f)
+        return text, labels
 
-        pass
-
-    def load_original_test(self):
+    def load_original_train(self):
         with open('train_text.pkd', 'rb') as f:
             text = dill.load(f)
         with open('train_label.pkd', 'rb') as f:
             labels = dill.load(f)
         return text, labels
-        pass
 
-    def load_train(self):
+    def load_unlabled(self):
+        with open('unsup_text.pkd', 'rb') as f:
+            text = dill.load(f)
+        return text
+
+
+    def load_augmented_test(self):
         pass
 
 
 if __name__ == "__main__":
     data = IMDBDataSet()
+    #Run once to create files
     #data.labled_to_pickle('train')
     #data.labled_to_pickle('test')
-    #data.random_original_review()
-    data.word_usage_analysis(1, 40)
+    #data.unlabled_to_pickle()
+
+    #data.random_unsup_review()
+    # data.random_original_review()
+    # data.word_usage_analysis(1, 40)
 
 
