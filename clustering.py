a51a040197436d52c2dcccacd042261d4c952c79
danulg <danulg@gmail.com>
2020-07-12 18:24:01 -0400

    Work started on clustering algorithm for semisupervised approach

--- clustering.py
+++ clustering.py
@@ -1,16 +1,29 @@
 import numpy as np
-import scipy
 from data_loader import IMDBDataSet
+from glove import LoadGloVe
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Embedding, Flatten
 import sklearn
+
+class Cluster():
+    def __init__(self):
+        super().__init__()
+
+class Encoder(Sequential):
+    def __init__(self, embedding_dim=100, max_words=100, maxlen=10000):
+        super().__init__()
+        self.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+        self.add(Flatten())
 
 
 if __name__=="__main__":
     data = IMDBDataSet()
     tr_dt, tr_lbl, word_index = data.load_data(name='train', is_numpy=True)
+    glove_weights =
     train_data = tr_dt[:20000]
     train_label = tr_lbl[:20000]
     val_data = tr_dt[20000:]
     val_label = tr_dt[20000:]
 
 
-embedding_dim = 100
+
--- network_architectures.py
+++ network_architectures.py
@@ -1,6 +1,23 @@
 from tensorflow.keras.layers import LSTM, Dense, Conv1D, Softmax, Flatten, Input, Embedding
 from tensorflow.keras import Sequential
 
+class sentiment_analysis_basic(Sequential):
+    def __int__(self, embedding_dim=100, max_words=100, maxlen=10000):
+        self.__int__(super)
+        self.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+        self.add(Flatten())
+        self.add(Dense(32, activation='relu'))
+        self.add(Dense(1, activation='sigmoid'))
+
+class sentiment_analysis_advanced(Sequential):
+    def __int__(self, embedding_dim=100, max_words=100, maxlen=10000):
+        self.__int__(super)
+        self.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+        self.add(Flatten())
+        self.add(Dense(32, activation='relu'))
+        self.add(Dense(1, activation='sigmoid'))
+
+#GANs
 class discrimintor_gan(Sequential):
     def __int__(self):
         self.__int__(super)
@@ -21,17 +38,3 @@ class generator_gan(Sequential):
         self.layer4 = self.add(Dense)
         self.layer5 = self.add(Softmax)
 
-class sentiment_analysis(Sequential):
-    def __int__(self):
-        self.__int__(super)
-
-class sentiment_analysis_pretrained(Sequential):
-    def __int__(self):
-        self.__int__(super)
-from keras.models import Sequential
-from keras.layers import Embedding, Flatten, Dense
-model = Sequential()
-model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
-model.add(Flatten())
-model.add(Dense(32, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
