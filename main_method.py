eab0e8a8dae8847c6f5582c9b14d231b85f0f235
danulg <danulg@gmail.com>
2020-07-22 12:35:05 -0400

    Issues related loading models resolved: Currently saving and loading models

--- main_method.py
+++ main_method.py
@@ -1,35 +1,39 @@
-#import and set randomseeds
+# import and set randomseeds
 import os
-os.environ['PYTHONHASHSEED']=str(12)
+
+os.environ['PYTHONHASHSEED'] = str(12)
 
 import tensorflow as tf
+
 tf.random.set_seed(324)
 
 import random
+
 random.seed(14)
 
 import numpy as np
 import numpy as np
+
 np.random.seed(15)
 
-#import rest of relavant libraries
+# import rest of relavant libraries
 from data_loader import IMDBDataSet
 from trainer import TrainNetworks
 from glove import LoadGloVe
 from compare_models import PlotCurves
 import dill
 
-#Prep GPU:
+# Prep GPU:
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print(gpus)
-tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(\
-     memory_limit=6000)])
+tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration( \
+    memory_limit=6000)])
 # tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(\
 #       memory_limit=3000)])
 logical = tf.config.experimental.list_logical_devices('GPU')
 print(logical[0])
 
-#Load and pratition data sets.
+# Load and pratition data sets.
 temp = IMDBDataSet()
 tr, lbl, words = temp.load_data()
 tr_dt = tr[:20000]
@@ -41,14 +45,14 @@ unsup, *_ = temp.load_data(name='unsup')
 temp = LoadGloVe(words)
 weights = temp.load_glove()
 
-#Various relavant parameters across the classes
-#Shared by all classes Data Loader, Glove, Network Architectures
+# Various relavant parameters across the classes
+# Shared by all classes Data Loader, Glove, Network Architectures
 maxlen = 100
 max_words = 10000
 embedding_dim = 100
-max_words = max_words #i.e. the parameters for glove and data loader should match
+max_words = max_words  # i.e. the parameters for glove and data loader should match
 
-#Specific to Convolutional archtecture of SingleConv1D
+# Specific to Convolutional archtecture of SingleConv1D
 filters = 64
 kernel_size = 5
 strides = 2
@@ -58,9 +62,9 @@ pool_size = 4
 epochs = 12
 iterates = 3
 sub_epochs = 4
-cutoff = 0.95
+cutoff = 0.9
 
-#Model parameters with default sizes
+# Model parameters with default sizes
 lstm_output_size = 128
 lstm_output_size = 128
 lstm_output_size2 = 128
@@ -69,22 +73,25 @@ rate = 0.6
 # # Create model trainer
 mod_trainer = TrainNetworks(tr_dt, tr_lbl, val_dt, val_lbl, unsup, weights)
 
-#Verify that epochs, iterates, sub epochs match up
-assert sub_epochs*iterates == epochs
+# Verify that epochs, iterates, sub epochs match up
+assert sub_epochs * iterates == epochs
 
-#Train neural network architecture: Basic
-basic_history, basic_model = mod_trainer.train(name='basic', epochs=epochs, rate=rate)
-glove_basic_history, glove_basic_model = mod_trainer.train(name='glove_basic', epochs=epochs, rate=rate)
-iter_basic_history, basic_iter_model = mod_trainer.train(name='basic', data='unlabled_considered',\
-                                        sub_epochs=sub_epochs, iterates=iterates, rate=rate, cutoff=cutoff)
+# Train neural network architecture: Basic
+basic_history, basic_model = mod_trainer.train(name='basic', epochs=epochs, sub_epochs=sub_epochs, iterates=iterates, \
+                                               rate=rate, cutoff=cutoff)
+glove_basic_history, glove_basic_model = mod_trainer.train(name='glove_basic', epochs=epochs, sub_epochs=sub_epochs,
+                                                           iterates=iterates, \
+                                                           rate=rate, cutoff=cutoff)
+iter_basic_history, basic_iter_model = mod_trainer.train(name='basic', data='unlabled_considered', \
+                                                         sub_epochs=sub_epochs, iterates=iterates, rate=rate,
+                                                         cutoff=cutoff)
 
 # Gather histories and save
 
 history = [basic_history, glove_basic_history, iter_basic_history]
 dill.dump(history, open('history_1.pkd', 'wb'))
 
-
-#Train neural network architecture: bidirectional. Rate is currently redundant as it has no dropout
+# Train neural network architecture: bidirectional. Rate is currently redundant as it has no dropout
 # bidirectional_history, bidirectional_model = mod_trainer.train(name='bidirectional', epochs=epochs, rate=rate)
 # glove_bidirectional_history, glove_bidirectional_model = mod_trainer.train(name='glove_bidirectional', epochs=epochs, rate=rate)
 # iter_bidirectional_history, bidirectional_iter_model = mod_trainer.train_unlabled(name='bidirectional', sub_epochs=sub_epochs,\
@@ -95,7 +102,9 @@ dill.dump(history, open('history_1.pkd', 'wb'))
 # history = [bidirectional_history, glove_bidirectional_history, iter_bidirectional_history]
 # dill.dump(history, open('history_bidirectional_4.pkd', 'wb'))
 
-#Draw plots
+# Code: Evaluate the models
+
+# Draw plots
 curves = PlotCurves()
 # curves.draw(dill.load(open('history_1.pkd', 'rb')), sub_epochs=sub_epochs, iterates=iterates)
 curves.bokeh_draw(dill.load(open('history_1.pkd', 'rb')), sub_epochs=sub_epochs, iterates=iterates)
--- trainer.py
+++ trainer.py
@@ -6,148 +6,146 @@ from network_architectures import SentimentAnalysisMultipleConv1D as MultipleCon
 
 
 class TrainNetworks():
-      def __init__(self, tr_dt, tr_lbl, val_dt, val_lbl, unsup, weights):
-          super().__init__()
-          #set class variables
-          self.tr_dt = tr_dt
-          self.tr_lbl = tr_lbl
-          self.val_dt = val_dt
-          self.val_lbl = val_lbl
-          self.val_lbl = val_lbl
-          self.glove_weights = weights
-          self.unlabled = unsup
+    def __init__(self, tr_dt, tr_lbl, val_dt, val_lbl, unsup, weights):
+        super().__init__()
+        # set class variables
+        self.tr_dt = tr_dt
+        self.tr_lbl = tr_lbl
+        self.val_dt = val_dt
+        self.val_lbl = val_lbl
+        self.val_lbl = val_lbl
+        self.glove_weights = weights
+        self.unlabled = unsup
 
-      def train(self, name='basic', data='labled_only', rate=0.5, lstm_output_size=128, lstm_output_size2=128, dense_output_size=128,
-                sub_epochs=4, iterates=2, epochs=8, batch_size=32, optimizer='adam', loss='binary_crossentropy',
-                metrics=['acc'], verbose=1, cutoff=0.8):
+    def train(self, name='basic', data='labled_only', rate=0.5, lstm_output_size=128, lstm_output_size2=128,
+              dense_output_size=128,
+              sub_epochs=4, iterates=2, epochs=8, batch_size=32, optimizer='adam', loss='binary_crossentropy',
+              metrics=['acc'], verbose=1, cutoff=0.8):
 
-          if name == 'basic':
-              model = Basic(rate=rate, dense_output_size=dense_output_size)
+        if name == 'basic':
+            model = Basic(rate=rate, dense_output_size=dense_output_size)
 
-          elif name == 'glove_basic':
-              model = Basic(rate=rate, dense_output_size=dense_output_size)
-              model.layers[0].set_weights([self.glove_weights])
-              model.layers[0].trainable = False
+        elif name == 'glove_basic':
+            model = Basic(rate=rate, dense_output_size=dense_output_size)
+            model.layers[0].set_weights([self.glove_weights])
+            model.layers[0].trainable = False
 
-          elif name == 'bidirectional':
-              model = Bidirectional(rate=rate, lstm_output_size=lstm_output_size, lstm_output_size2=lstm_output_size2)
+        elif name == 'bidirectional':
+            model = Bidirectional(rate=rate, lstm_output_size=lstm_output_size, lstm_output_size2=lstm_output_size2)
 
-          elif name == 'glove_bidirectional':
-              model = Bidirectional(rate=rate)
-              model.layers[0].set_weights([self.glove_weights])
-              model.layers[0].trainable = False
+        elif name == 'glove_bidirectional':
+            model = Bidirectional(rate=rate)
+            model.layers[0].set_weights([self.glove_weights])
+            model.layers[0].trainable = False
 
-          elif name == 'conv':
-              model = SingleConv1D(rate=rate)
+        elif name == 'conv':
+            model = SingleConv1D(rate=rate)
 
-          elif name == 'glove_conv':
-              model = SingleConv1D(rate=rate)
-              model.layers[0].set_weights([self.glove_weights])
-              model.layers[0].trainable = False
+        elif name == 'glove_conv':
+            model = SingleConv1D(rate=rate)
+            model.layers[0].set_weights([self.glove_weights])
+            model.layers[0].trainable = False
 
-          elif name == 'conv_md':
-              model = MultipleConv1D()
+        elif name == 'conv_md':
+            model = MultipleConv1D()
 
-          elif name == 'glove_conv_md':
-              model = MultipleConv1D(rate=rate)
-              model.layers[0].set_weights([self.glove_weights])
-              model.layers[0].trainable = False
+        elif name == 'glove_conv_md':
+            model = MultipleConv1D(rate=rate)
+            model.layers[0].set_weights([self.glove_weights])
+            model.layers[0].trainable = False
 
-          else:
-              print("Type of model not identified")
-              return 0, 0
+        else:
+            print("Type of model not identified")
+            return 0, 0
 
-          model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+        model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+
+        if data == 'labled_only':
+            save_name = str(cutoff) + '_' + name + '_model_' + str(sub_epochs) + '_' + str(iterates) + '_' + str(
+                dense_output_size) + '_' \
+                        + str(lstm_output_size) + '_' + str(lstm_output_size2) +'.h5'
+            history = model.fit(self.tr_dt, self.tr_lbl, epochs=epochs, batch_size=batch_size,
+                                validation_data=(self.val_dt, \
+                                                 self.val_lbl), verbose=verbose)
+            model.save_weights(save_name)
+            return history.history, model
+
+        elif data == 'unlabled_considered':
+            i = 0
+            history = []
+            save_name = str(cutoff) + '_' + name + '_itermodel_' + str(sub_epochs) + '_' + str(iterates) + '_' + str(
+                dense_output_size) + \
+                        '_' + str(lstm_output_size) + '_' + str(lstm_output_size2) + '.h5'
+            while (i < iterates):
+                print("iteration cycle:", i + 1)
+                temp = model.fit(self.tr_dt, self.tr_lbl, batch_size=batch_size, epochs=sub_epochs, \
+                                 validation_data=(self.val_dt, self.val_lbl), verbose=verbose)
+                history.append(temp.history)
+                self.__add_remove(model, cutoff)
+                print('The number of training examples for iterate ' + str(i) + ' is:', len(self.tr_dt))
+                print('The number of unlabled examples left:', len(self.unlabled))
+                i += 1
+            # Code for saving the model: Cannot load models this way: known tensorflow error
+            # model.save(save_name)
+            model.save_weights(save_name)
+            return history, model
+
+    def __add_remove(self, model, cutoff):
+        predicitons = model.predict(self.unlabled)
+
+        # Adds high confidence results to training.
+        # Add more iterations and modify to keep track of added unlabled data
+        i = 0
+        np_one = np.array([1])
+        np_zero = np.array([0])
+        to_remove = []
+
+        for x in np.nditer(predicitons):
+            if x >= cutoff:
+                self.tr_dt = np.append(self.tr_dt, np.array([self.unlabled[i]]), axis=0)
+                self.tr_lbl = np.append(self.tr_lbl, np_one)
+                to_remove.append(i)
+                i += 1
+            elif x <= 1 - cutoff:
+                self.tr_dt = np.append(self.tr_dt, np.array([self.unlabled[i]]), axis=0)
+                self.tr_lbl = np.append(self.tr_lbl, np_zero)
+                to_remove.append(i)
+                i += 1
+            else:
+                i += 1
+
+        self.unlabled = np.delete(self.unlabled, to_remove, axis=0)
+
+# Unsupervised: Ideas include running through half trained and adding and taking out high threshold data!
+# Note down times for training.
+
+# Model 1: On the 25000 smaples - Basic
 
 
-          if data == 'labled_only':
-              save_name = str(cutoff)+'_' + name + '_model_' + str(sub_epochs) + '_' + str(iterates) + '_' + str(dense_output_size) + '_'\
-                          + str(lstm_output_size) + '_' + str(lstm_output_size2)
-              history = model.fit(self.tr_dt, self.tr_lbl, epochs=epochs, batch_size=batch_size, validation_data=(self.val_dt, \
-                                self.val_lbl), verbose=verbose)
-              model.save(save_name)
-              return history.history, model
+# Model 2: Transfer learning with Glove but no interaction with cluseteing or unsupervised set: all examples 25000  - Basic
 
-          elif data=='unlabled_considered':
-              i = 0
-              history = []
-              save_name = str(cutoff)+'_'+name + '_itermodel_' + str(sub_epochs) + '_' + str(iterates) + '_' + str(dense_output_size) +\
-                          '_' + str(lstm_output_size) + '_' + str(lstm_output_size2)
-              while (i < iterates):
-                  print("iteration cycle:", i+1)
-                  temp = model.fit(self.tr_dt, self.tr_lbl, batch_size=batch_size, epochs=sub_epochs, \
-                                   validation_data=(self.val_dt, self.val_lbl), verbose=verbose)
-                  history.append(temp.history)
-                  self.__add_remove(model, cutoff)
-                  print('The number of training examples for iterate ' + str(i) + ' is:', len(self.tr_dt))
-                  print('The number of unlabled examples left:', len(self.unlabled))
-                  i += 1
+# Model 3:  Transfer learning with Glove but no interaction with cluseteing or unsupervised set: with a fraction of the original data- Basic
 
-              model.save(save_name)
-              return history, model
+# Model 4: Clustering with glove6B to add additional examples - Basic
 
+# Model 5: Train in cycles to add / remove examples with high probability - Basic
 
-      def __add_remove(self, model, cutoff):
-          predicitons = model.predict(self.unlabled)
+# Model 6: On the 25000 smaples - Advanced
 
-          # Adds high confidence results to training.
-          # Add more iterations and modify to keep track of added unlabled data
-          i = 0
-          np_one = np.array([1])
-          np_zero = np.array([0])
-          to_remove = []
+# Model 7: Transfer learning with Glove but no interaction with cluseteing or unsupervised set: all examples 25000  - Advanced
 
-          for x in np.nditer(predicitons):
-              if x >= cutoff:
-                  self.tr_dt = np.append(self.tr_dt, np.array([self.unlabled[i]]), axis=0)
-                  self.tr_lbl = np.append(self.tr_lbl, np_one)
-                  to_remove.append(i)
-                  i += 1
-              elif x <= 1-cutoff:
-                  self.tr_dt = np.append(self.tr_dt, np.array([self.unlabled[i]]), axis=0)
-                  self.tr_lbl = np.append(self.tr_lbl, np_zero)
-                  to_remove.append(i)
-                  i += 1
-              else:
-                  i += 1
+# Model 8:  Transfer learning with Glove but no interaction with cluseteing or unsupervised set: with a fraction - Advanced
 
-          self.unlabled = np.delete(self.unlabled, to_remove, axis=0)
+# Model 9: Clustering with glove6B to add additional examples - Advanced
 
-
-#Unsupervised: Ideas include running through half trained and adding and taking out high threshold data!
-#Note down times for training.
-
-#Model 1: On the 25000 smaples - Basic
-
-
-
-
-#Model 2: Transfer learning with Glove but no interaction with cluseteing or unsupervised set: all examples 25000  - Basic
-
-#Model 3:  Transfer learning with Glove but no interaction with cluseteing or unsupervised set: with a fraction of the original data- Basic
-
-#Model 4: Clustering with glove6B to add additional examples - Basic
-
-#Model 5: Train in cycles to add / remove examples with high probability - Basic
-
-#Model 6: On the 25000 smaples - Advanced
-
-#Model 7: Transfer learning with Glove but no interaction with cluseteing or unsupervised set: all examples 25000  - Advanced
-
-#Model 8:  Transfer learning with Glove but no interaction with cluseteing or unsupervised set: with a fraction - Advanced
-
-#Model 9: Clustering with glove6B to add additional examples - Advanced
-
-#Model 10: Train in cycles to add / remove examples with high probability - Advanced
+# Model 10: Train in cycles to add / remove examples with high probability - Advanced
 
 ############# IDEAL: 5, 4 > 2 > 3 > 1 AND 10, 9 > 7 > 8 > 6
 
-#Possibly a fifth model that interacts with clustering
+# Possibly a fifth model that interacts with clustering
 
 # for x in self.unlabled:
 #     y = model.predict(x)
 #
 #     if y>=0.8:
 #        np.append(self.tr_dt, x, axis=0)
-
-
