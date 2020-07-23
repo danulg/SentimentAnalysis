735ac35a4beb8605860a4b2706dad52f4f45936d
danulg <danulg@gmail.com>
2020-07-23 13:57:49 -0400

    code rewritten to make changing maxlen, max_words easier

--- data_loader.py
+++ data_loader.py
@@ -15,7 +15,7 @@ class IMDBDataSet():
         self.amazingly_sub = {'amazingly', 'stunningly', 'astoundlingly'}
         self.love_sub = {'love', 'adore'}
         self.great_sub = {'great', 'really nice'}
-        self.imdb_dir = '/home/danulg/IMDB'
+        self.imdb_dir = './IMDB'
         self.nlp = spacy.load('en_core_web_sm')
         self.stopwords = self.nlp.Defaults.stop_words
 
@@ -24,7 +24,7 @@ class IMDBDataSet():
         #To be added
         pass
 
-    def pickle_data(self, name='train'):
+    def __load_from_source(self, name='train'):
         labels = []
         text = []
         dir = os.path.join(self.imdb_dir, name)
@@ -34,32 +34,29 @@ class IMDBDataSet():
                 for fname in os.listdir(dir_name):
                     with open(os.path.join(dir_name, fname)) as f:
                         x = f.read()
-                        x = self.punc_strip(x)
+                        x = self.__punc_strip(x)
                         text.append(x)
 
                     if label_type == 'neg':
                         labels.append(0)
                     else:
                         labels.append(1)
-        # Check number of examples
-            print(type(text), len(text))
-            print(type(labels), len(labels))
-            dill.dump(text, open(name + '_text.pkd', 'wb'))
-            dill.dump(labels, open(name + '_label.pkd', 'wb'))
+
+            return text, labels
 
         elif name == 'unsup':
             for fname in os.listdir(dir):
                 with open(os.path.join(dir, fname)) as f:
                     x = f.read()
-                    x = self.punc_strip(x)
+                    x = self.__punc_strip(x)
                     text.append(x)
 
-            print(type(text), len(text))
-            dill.dump(text, open('unsup_text.pkd', 'wb'))
+            return text, labels
+
 
 
     #Method for stripping punctuation etc
-    def punc_strip(self, x):
+    def __punc_strip(self, x):
         x = x.lower()
         x = re.sub(r'\<br', '', x)
         x = re.sub(r'\/br\>', '', x)
@@ -70,72 +67,51 @@ class IMDBDataSet():
 
     #Methods for looking at random data
     def random_review(self, name='train'):
-        name = name + '_text.pkd'
-        text, labels,_ = self.load_data(is_numpy=False)
+        text, labels = self.__load_from_source(name=name)
         print(name, len(text))
         i = 0
 
         while(i<10):
             j = random.randint(0, len(text) - 1)
-            if name == 'unsup_text.pkd':
+            if name == 'unsup':
                 print(text[j], ': unlabled')
                 i+=1
             else:
                 print(text[j], ':', labels[j])
                 i+=1
 
-    def remove_stopwords(self, sentence):
-        tokens = sentence.split(" ")
-        tokens_filtered = [word for word in tokens if not word in self.stopwords]
-        return (" ").join(tokens_filtered)
+    def __remove_stopwords(self, text):
+        stripped_text = []
+        for review in text:
+            tokens = review.split(" ")
+            tokens_filtered = [word for word in tokens if not word in self.stopwords]
+            tokens_filtered = (" ").join(tokens_filtered)
+            stripped_text.append(tokens_filtered)
 
+        return stripped_text
 
-    def pickle_data_stopwords(self, name='train'):
-        save_name = name + '_text_stopwords.pkd'
-        name = name + '_text.pkd'
-        with open(name, 'rb') as f:
-            text = dill.load(f)
-
-        text = [self.remove_stopwords(x) for x in text]
-
-        dill.dump(text, open(save_name, 'wb'))
-
-    def load_data(self, name='train', is_numpy=True, has_stopwords=True, maxlen=100, max_words=10000):
-        #Track labels as required
-        if name == 'train' or name == 'test':
-            lbl = name + '_label.pkd'
-            with open(lbl, 'rb') as f:
-                labels = dill.load(f)
-        else:
-            labels = []
-
+    def load_data(self, name='train', is_numpy=True, has_stopwords=True, maxlen=200, max_words=30000):
         #Format data based on return type
+        text, labels = self.__load_from_source(name=name)
+
         if is_numpy:
-            if has_stopwords:
-                name = name + '_text.pkd'
+            if not has_stopwords:
+                sequences, word_index = self.__tokenize(text, max_words=max_words)
 
             else:
-                name = name + '_text_nostopwords.pkd'
+                text = self.__remove_stopwords(text)
+                sequences, word_index = self.__tokenize(text, max_words=max_words)
 
-            with open(name, 'rb') as f:
-                text = dill.load(f)
-
-            sequences, word_index = self.__tokenize(text, max_words=max_words)
             data, labels = self.__data_to_numpy(sequences, labels, maxlen)
             return data, labels, word_index
 
-        else:
-            name = name + '_text.pkd'
-            with open(name, 'rb') as f:
-                text = dill.load(f)
 
+        else:
             _, word_index = self.__tokenize(text, max_words=max_words)
             return text, labels, word_index
 
 
-
-
-    def __data_to_numpy(self, sequences, labels, maxlen=100):
+    def __data_to_numpy(self, sequences, labels=[], maxlen=100):
         #Convert to numpy
         data = pad_sequences(sequences, maxlen=maxlen)
         print('Shape of data tensor:', data.shape)
@@ -164,12 +140,5 @@ class IMDBDataSet():
 if __name__ == "__main__":
     data = IMDBDataSet()
 
-    #Create files for easier loading
-    data.pickle_data(name='train')
-    data.pickle_data(name='test')
-    data.pickle_data(name='unsup')
 
-    data.pickle_data_stopwords(name='train')
-    data.pickle_data_stopwords(name='test')
-    data.pickle_data_stopwords(name='unsup')
 
