3e7062f58b7a59650b28e21e16cf27b251b7eee4
danulg <danulg@gmail.com>
2020-07-28 18:01:53 -0400

    Baseline finalized

--- baseline.py
+++ baseline.py
@@ -14,12 +14,12 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import SGDClassifier
 
 import dill
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import SGDClassifier
 from dataloader import IMDBDataSet
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
@@ -33,14 +33,14 @@ if __name__ == '__main__':
     text, labels = imdb.reviews(ret_val=True)
 
     train_dt, test_dt, train_lb, test_lb = train_test_split(text, labels, test_size=0.2, random_state=42)
-    bag_of_words_est = Pipeline([('cvt', CountVectorizer(min_df=0.005)), ('SGD', SGDClassifier())])
+    bag_of_words_est = Pipeline([('cvt', CountVectorizer(min_df=0.005)), ('mb', MultinomialNB())])
 
     bag_of_words_est.fit(train_dt, train_lb)
     dill.dump(bag_of_words_est, open('bag_of_words_est', 'wb'))
 
     y_pred = bag_of_words_est.predict(test_dt)
 
-    # error = mean_squared_error(y_pred, test_lb)
-    #
-    # print(error)
+    error = accuracy_score(y_pred, test_lb)
+
+    print(error)
 
