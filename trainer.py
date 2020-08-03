95e028423c24e7eea4e6446b4d3c95b8c0c4b924
danulg <danulg@gmail.com>
2020-08-03 17:57:16 -0400

    code refactored for redability

--- trainer.py
+++ trainer.py
@@ -1,70 +1,70 @@
 import numpy as np
-from architectures import SentimentAnalysisBasic as Basic
-from architectures import SentimentAnalysisBidirectional as Bidirectional
-
+from architectures import AnalysisBasic as Basic
+from architectures import AnalysisBidirectional as Bidirectional
+from architectures import ConvolutionalLSTM as ConvLSTM
 
 
 class TrainNetworks():
-    def __init__(self, tr_dt, tr_lbl, val_dt, val_lbl, weights):
+    def __init__(self, tr_dt, tr_lbl, val_dt, val_lbl, glove_weights, w2vec_weights, optimizer='adam',
+                 loss='binary_crossentropy', metrics=['acc']):
         super().__init__()
         # set class variables
         self.tr_dt = tr_dt
         self.tr_lbl = tr_lbl
         self.val_dt = val_dt
         self.val_lbl = val_lbl
-        self.val_lbl = val_lbl
-        self.glove_weights = weights
-        self.optimizer = 'adam'
-        self.loss = 'binary_crossentropy'
-        self.metrics = ['acc']
+        self.glove_weights = glove_weights
+        self.w2vec_weights = w2vec_weights
+        self.optimizer = optimizer
+        self.loss = loss
+        self.metrics = metrics
 
-
-    def train(self, name='basic', rate=0.5, lstm_output_size=512, lstm_output_size2=512,
-              dense_output_size=128, epochs=12, batch_size=32, verbose=1):
+    def train(self, name='basic', rate=0.5, lstm_output_size=128, lstm_output_size2=128, dense_output_size=128,
+              epochs=12, batch_size=32, verbose=1):
 
         if name == 'basic':
             model = Basic(rate=rate, dense_output_size=dense_output_size)
+            save_name = name + '_model_' + str(epochs) + '_' + str(dense_output_size) + '_' + str(rate) + '.h5'
 
         elif name == 'glove_basic':
             model = Basic(rate=rate, dense_output_size=dense_output_size)
             model.layers[0].set_weights([self.glove_weights])
             model.layers[0].trainable = False
+            save_name = name + '_model_' + str(epochs) + '_' + str(dense_output_size) + '_' + str(rate) + '.h5'
+
+        elif name == 'w2vec_basic':
+            model = Basic(rate=rate, dense_output_size=dense_output_size)
+            model.layers[0].set_weights([self.w2vec_weights])
+            model.layers[0].trainable = False
+            save_name = name + '_model_' + str(epochs) + '_' + str(dense_output_size) + '_' + str(rate) + '.h5'
 
         elif name == 'bidirectional':
             model = Bidirectional(rate=rate, lstm_output_size=lstm_output_size, lstm_output_size2=lstm_output_size2)
+            save_name = name + '_model_' + str(epochs) + '_' + str(dense_output_size) + '_' + str(rate) + '_' + \
+                        str(lstm_output_size) + '_' + str(lstm_output_size2) + '.h5'
 
         elif name == 'glove_bidirectional':
-            model = Bidirectional(rate=rate)
+            model = Bidirectional(rate=rate, lstm_output_size=lstm_output_size, lstm_output_size2=lstm_output_size2)
             model.layers[0].set_weights([self.glove_weights])
             model.layers[0].trainable = False
+            save_name = name + '_model_' + str(epochs) + '_' + str(dense_output_size) + '_' + str(rate) + '_' + \
+                        str(lstm_output_size) + '_' + str(lstm_output_size2) + '.h5'
 
-        elif name == 'conv':
-            model = SingleConv1D(rate=rate)
-
-        elif name == 'glove_conv':
-            model = SingleConv1D(rate=rate)
-            model.layers[0].set_weights([self.glove_weights])
+        elif name == 'w2vec_bidirectional':
+            model = Bidirectional(rate=rate, lstm_output_size=lstm_output_size, lstm_output_size2=lstm_output_size2)
+            model.layers[0].set_weights([self.w2vec_weights])
             model.layers[0].trainable = False
-
-        elif name == 'conv_md':
-            model = MultipleConv1D()
-
-        elif name == 'glove_conv_md':
-            model = MultipleConv1D(rate=rate)
-            model.layers[0].set_weights([self.glove_weights])
-            model.layers[0].trainable = False
+            save_name = name + '_model_' + str(epochs) + '_' + str(dense_output_size) + '_' + str(rate) + '_' + \
+                        str(lstm_output_size) + '_' + str(lstm_output_size2) + '.h5'
 
         else:
             print("Type of model not identified")
             return 0, 0
 
-        model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
         model.summary()
 
-        save_name = str(cutoff) + '_' + name + '_model_' + str(epochs) + '_' + str(
-            dense_output_size) + '_' + str(rate) + '_' + str(lstm_output_size) + '_' + str(lstm_output_size2) + '.h5'
         history = model.fit(self.tr_dt, self.tr_lbl, epochs=epochs, batch_size=batch_size,
                             validation_data=(self.val_dt, self.val_lbl), verbose=verbose)
         model.save_weights(save_name)
         return history.history, model
-
