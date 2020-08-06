e19cbf77e0d0d3fe8e5e337b1e29c4a2be562f7c
danulg <danulg@gmail.com>
2020-08-06 15:10:30 -0400

    Code for evaluation modified to fit modifications to other modules

--- evalmod.py
+++ evalmod.py
@@ -1,29 +1,30 @@
-from tensorflow import keras
-from data_loader import IMDBDataSet
 import tensorflow as tf
-from network_architectures import SentimentAnalysisBasic
+from dataloader import IMDBDataSet
+from architectures import AnalysisBasic as Basic
+from architectures import AnalysisBidirectional as Bidirectional
+from architectures import ConvolutionalLSTM as ConvLSTM
 
 if __name__ == '__main__':
-    #Create new model: Parameters should match with model being loaded
-    model_arch = SentimentAnalysisBasic(rate=0.6)
-    model_arch.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    model_arch.load_weights('0.9_basic_model_4_3_128_128_128.h5')
+    max_words = 20000
+    embedding_dim = 100
+    # Prep GPU:
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    print(gpus)
+    tf.config.experimental.set_virtual_device_configuration(gpus[0],
+                                                            [tf.config.experimental.VirtualDeviceConfiguration(
+                                                                memory_limit=6000)])
+    logical = tf.config.experimental.list_logical_devices('GPU')
+    print(logical[0])
 
-    model_glove = SentimentAnalysisBasic(rate=0.6)
-    model_glove.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    model_glove.load_weights('0.9_glove_basic_model_4_3_128_128_128.h5')
+    # Load data
+    imdb = IMDBDataSet()
+    seq_text, labels, _ = imdb.load_data_default(name='test')
 
-    model_iter = SentimentAnalysisBasic(rate=0.6)
-    model_iter.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    model_iter.load_weights('0.9_basic_itermodel_4_3_128_128_128.h5')
-
-    # model_iter = keras.models.load_model('0.95_basic_itermodel_4_3_128_128_128')
-    temp = IMDBDataSet()
-    test_dt, test_lbl, _ = temp.load_data(name='test')
-
-    model_arch.evaluate(test_dt, test_lbl)
-    model_glove.evaluate(test_dt, test_lbl)
-    model_iter.evaluate(test_dt, test_lbl)
+    model = ConvLSTM()
+    model.build((max_words, embedding_dim))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    model.load_weights('conv_conv_lstm_model_100_32128_0.5.h5')
+    model.evaluate(seq_text, labels)
 
 # Code for reloading saved tokenizer and different approaches to saving tokenizer
 # =========================================================================================================================
